431c9605e548e616f4596364a00a5536b93ba7cc
jrmckim <jrmckim@mun.ca>
2022-11-26 15:07:21 -0330

    Changed values to vectors

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ fps = 60
 
 # Player Values
 p1size = [30,50]
-p1speed = [10,10]
+p1speed = 10
 p1pos = [displaysize[0]/2,displaysize[1]/2]
 
 # Displaying Text
@@ -33,13 +33,9 @@ def message (msg,colour,x,y):
     mesg = font.render(msg, True, colour)
     dis.blit(mesg, [x, y])
 def move_player(speed, position, joystick):
-
+    position[0] += joystick[0] * speed
+    position[1] += joystick[1] * speed
 while not game_over:
-
-    #Visual output
-    dis.fill(black)
-    pygame.draw.rect(dis, red, [x, y, psizex, psizey])
-    pygame.display.update()
 
     # Closes Window if X is pressed
     for event in pygame.event.get():
@@ -49,7 +45,7 @@ while not game_over:
 
     # Visual output
     dis.fill(black)
-    pygame.draw.rect(dis, red, [x, y, psizex, psizey])
+    pygame.draw.rect(dis, red, [p1pos[0], p1pos[1], p1size[0], p1size[1]])
     pygame.display.update()
 
 pygame.quit()
