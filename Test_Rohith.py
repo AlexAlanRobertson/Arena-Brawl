f1facf9eb9114c9a4c95a78b0c0a04bec975f695
jrmckim <jrmckim@mun.ca>
2022-11-26 17:03:34 -0330

    Bullets!

--- Test_Rohith.py
+++ Test_Rohith.py
@@ -1,10 +1,15 @@
 import pygame
+from engi1020.arduino.api import *
+from time import sleep
+import random
+from math import *
 
 pygame.init()
 dis = pygame.display.set_mode((800, 600))
 pygame.display.update()
 pygame.display.set_caption("Dodge")
 game_over = False
+fps = pygame.time.Clock()
 
 # colours
 blue = (0, 0, 255)
@@ -13,90 +18,84 @@ red = (255, 0, 0)
 black = (0, 0, 0)
 yellow = (255,255,0)
 
-fps = 60
-# starting position and display
-disx = 800
-disy = 600
-dis = pygame.display.set_mode((disx, disy))
+# display
+displaysize = [800,600]
+dis = pygame.display.set_mode((displaysize[0], displaysize[1]))
+startup_screen = True
 
+# Player Values
 p1size = [30,50]
-p1speed = [10,10]
-p1lives = 3
-p1cooldown = 0
-p1pos = [disx/1.5,disy/2]
+p1speed = 4
+p1pos = [displaysize[0]/2,displaysize[1]/2]
+p1bullets = []
 
-p2size = [30,50]
-p2speed = [10,10]
-p2lives = 3
-p2cooldown = 0
-p2pos = [disx/4,disy/2]
+# Bullet Values
+bulletsize = 5
+bulletspeed = 15
 
-clock = pygame.time.Clock()
-timer = 10
+# Displaying Text
 font = pygame.font.Font('freesansbold.ttf', 32)
 def message (msg,colour,x,y):
     mesg = font.render(msg, True, colour)
     dis.blit(mesg, [x, y])
+def move_player(speed, position, joystick):
+    position[0] += joystick[0] * speed
+    position[1] += joystick[1] * speed
+    if position[0] > displaysize[0] - p1size[0]:
+        position[0] = displaysize[0] - p1size[0]
+    if position[0] < 0:
+        position[0] = 0
+    if position[1] > displaysize[1] - p1size[1]:
+        position[1] = displaysize[1] - p1size[1]
+    if position[1] < 0:
+        position[1] = 0
+    return position
+
+def move_bullet(speed, position, angle):
+    position[0] += speed * cos(angle)
+    position [1] += speed * sin(angle)
+    return position
+
+
 
 while not game_over:
-    # Closes window
+    # Closes Window if X is pressed
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
-
     keys = pygame.key.get_pressed()
 
+#Joystick Movement
+    joystick = [0, 0]
+    if keys[pygame.K_w]:
+        joystick[1] = -1
+    elif keys[pygame.K_s]:
+        joystick[1] = 1
+    if keys[pygame.K_a]:
+        joystick[0] = -1
+    elif keys[pygame.K_d]:
+        joystick[0] = 1
+    #joystick.append(joystick_get_x())
+    #joystick.append(joystick_get_y())
+    print(joystick)
+
+#Move entities
+    p1pos = move_player(p1speed, p1pos, joystick)
+    if keys[pygame.K_SPACE]:
+        bulletposition = [p1pos[0]+p1size[0]/2,p1pos[1] + p1size[1]/2]
+        p1bullets.append([bulletposition,0.8])
+
+
+
+
+    # Visual output
     dis.fill(black)
     pygame.draw.rect(dis, red, [p1pos[0], p1pos[1], p1size[0], p1size[1]])
-    pygame.draw.rect(dis, blue, [p2pos[0], p2pos[1], p2size[0], p2size[1]])
+    for bullet in p1bullets:
+        new_pos = move_bullet(bulletspeed,bullet[0],bullet[1])
+        pygame.draw.circle(dis, white,new_pos, bulletsize )
+
     pygame.display.update()
-
-#PLAYER ONE MOVEMENT AND ABLIITY
-    if keys[pygame.K_SPACE] and p1cooldown == 0:
-        p1speed[0] = 200
-        p1speed[1]= 200
-    if keys[pygame.K_a]:
-        p1pos[0] += -p1speed[0]
-        if keys[pygame.K_s] or keys[pygame.K_w]:
-            p1pos[0] += 0.414 * p1speed
-        p1pos[0] = check_x(p1pos[0], p1sizex)
-    if keys[pygame.K_d]:
-        p1pos[0] += p1speed
-        if keys[pygame.K_s] or keys[pygame.K_w]:
-            p1pos[0] += 0.414 * -p1speed
-        p1pos[0] = check_x(p1pos[0], p1sizex)
-    if keys[pygame.K_w]:
-        p1pos[1] += -p1speed
-        if keys[pygame.K_d] or keys[pygame.K_a]:
-            p1pos[1] += 0.414 * p1speed
-        p1pos[1] = check_y(y1,p1sizey)
-    if keys[pygame.K_s]:
-        p1pos[1] += p1speed
-        if keys[pygame.K_d] or keys[pygame.K_a]:
-            y1 += 0.414 * -p1speed
-        p1pos[1] = check_y(y1,p1sizey)
-
-#PLAYER TWO MOVEMENT AND ABILITYs
-    if keys[pygame.K_LEFT]:
-        x2 += -p2speed
-        if keys[pygame.K_DOWN] or keys[pygame.K_UP]:
-            x2 += 0.414 * p2speed
-        x2 = check_x(x2, p2sizex)
-    if keys[pygame.K_RIGHT]:
-        x2 += p2speed
-        if keys[pygame.K_s] or keys[pygame.K_w]:
-            x2 += 0.414 * -p2speed
-        x2 = check_x(x2, p2sizex)
-    if keys[pygame.K_UP]:
-        y2 += -p2speed
-        if keys[pygame.K_RIGHT] or keys[pygame.K_LEFT]:
-            y2 += 0.414 * p2speed
-        y2 = check_y(y2,p2sizey)
-    if keys[pygame.K_DOWN]:
-        y2 += p2speed
-        if keys[pygame.K_RIGHT] or keys[pygame.K_LEFT]:
-            y2 += 0.414 * -p2speed
-        y2 = check_y(y2,p2sizey)
-
+    fps.tick(60)
 pygame.quit()
 quit()
